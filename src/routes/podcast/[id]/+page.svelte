9f570d88de9e56e07d743cfd4951ab4131267eb9
Justin Mandzik <justin@azath.dev>
2023-10-04 00:55:40 -0400

    podcast page tweaks

--- src/routes/podcast/[id]/+page.svelte
+++ src/routes/podcast/[id]/+page.svelte
@@ -4,4 +4,18 @@
 	console.log(data);
 </script>
 
-<img src={data.podcast.image} alt={data.podcast.title} style:--tag={`p-${data.podcast.id}`} />
+<div class="container">
+	<img src={data.podcast.image} alt={data.podcast.title} style:--tag={`p-${data.podcast.id}`} />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		overflow: auto;
+		width: 100%;
+		height: 100%;
+	}
+</style>
